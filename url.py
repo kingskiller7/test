38a68bfa29eb9431598e38da598a828616edd6c8
kingskiller <93809757+kingskiller7@users.noreply.github.com>
2023-11-26 18:35:56 +0530

    Update url.py

--- url.py
+++ url.py
@@ -1,28 +1,33 @@
 import string
 import random
+import requests
 
-url_mapping = {}  # Dictionary to store the URL mappings
+url_db = {}
 
 def generate_short_url():
-    characters = string.ascii_letters + string.digits  # Generate a combination of letters and digits
-    short_url = ''.join(random.choice(characters) for _ in range(6))  # Generate a 6-character short URL
+    chars = string.ascii_letters + string.digits
+    short_url = ''.join(random.choice(chars) for _ in range(6))
     return short_url
 
-def shorten_url(original_url):
+def shorten_url(long_url):
     short_url = generate_short_url()
-    url_mapping[short_url] = original_url
-    return short_url
+    url_db[short_url] = long_url
+    return f'Short URL: http://localhost:5000/{short_url}'
 
-def expand_url(short_url):
-    if short_url in url_mapping:
-        return url_mapping[short_url]
+def redirect_to_long_url(short_url):
+    long_url = url_db.get(short_url)
+    if long_url is None:
+        return 'Invalid short URL'
     else:
-        return None
+        return requests.get(long_url).content
 
-# Example usage:
-original_url = "https://www.example.com"
-shortened_url = shorten_url(original_url)
-print("Shortened URL:", shortened_url)
+if __name__ == '__main__':
+    while True:
+        long_url = input("Enter the long URL to shorten (or 'exit' to quit): ")
+        if long_url.lower() == 'exit':
+            break
+        else:
+            short_url = shorten_url(long_url)
+            print(short_url)
 
-expanded_url = expand_url(shortened_url)
-print("Expanded URL:", expanded_url)
+    print('Thank you for using the URL shortener service!')
