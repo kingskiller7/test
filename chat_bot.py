841a77fe1a02f2daf95767e887c5fa8aa945aee4
kingskiller <93809757+kingskiller7@users.noreply.github.com>
2023-11-26 18:47:30 +0530

    Update chat_bot.py

--- chat_bot.py
+++ chat_bot.py
@@ -1,21 +1,31 @@
 import nltk
 from nltk.chat.util import Chat, reflections
 
-# Define some simple patterns and responses
 patterns = [
     (r'hi|hello|hey', ['Hello!', 'Hi there!', 'Hey!']),
     (r'how are you', ['I am good, thank you.', 'I am doing well.']),
-    (r'quit', ['Goodbye!', 'It was nice talking to you.']),
+    (r'what is your name', ['I am a chatbot. You can call me ChatGPT.']),
+    (r'your age', ['I don\'t have an age. I\'m just a program.']),
+    (r'who created you', ['I was created by a team of developers.']),
+    (r'programming|code', ['I can help you with programming questions.']),
+    (r'weather', ['I don\'t have real-time data, but you can check a weather website.']),
+    (r'time', ['I don\'t have real-time clock access.']),
+    (r'bye|goodbye', ['Goodbye!', 'It was nice talking to you.']),
+    (r'your favorite color', ['I don\'t have preferences. I am just a program.']),
+    (r'joke|funny', ['Why don\'t scientists trust atoms? Because they make up everything!']),
+    (r'open Google', ['I can\'t open websites.']),
+    (r'help', ['I can assist you with general questions. Try asking me something!']),
+    # Add more patterns and responses here...
 ]
 
 # Create a chatbot with the defined patterns
 chatbot = Chat(patterns, reflections)
 
 # Start the conversation
-print("Hello! I'm a simple chatbot. Type 'quit' to exit.")
+print("Hello! I'm a simple chatbot. Type 'bye' to exit.")
 while True:
     user_input = input("You: ")
-    if user_input.lower() == 'quit':
+    if user_input.lower() == 'bye':
         print("Goodbye!")
         break
     else:
